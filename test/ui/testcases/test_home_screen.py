b7281950f97dab39bd98945e2cd9bcd52d545f32
ETimur <32924313+ETimur@users.noreply.github.com>
2024-11-28 08:38:02 +0600

    Update test_home_screen.py

--- test/ui/testcases/test_home_screen.py
+++ test/ui/testcases/test_home_screen.py
@@ -75,14 +75,9 @@ class TestHomeScreen(BaseTest):
         self.home_screen_page.open_the_app()
         self.home_screen_page.login_with_password(pytest.provider_email, self.data.reset_password)
 
-assert self.home_screen_page.is_element_visible(self.appointment_screen_page.TRACKER_TAB, 5)
-            # assert self.home_screen_page.is_element_visible(self.appointment_screen_page.TO_DO_TAB, 1)
-    
-
-    
-
-     # with allure.step('Appointment screen should be displayed with Tracker, To-DO tabs'):
-            
+# Assert visibility of key UI elements
+        assert self.home_screen_page.is_element_visible(self.appointment_screen_page.TRACKER_TAB, 5)
+        # assert self.home_screen_page.is_element_visible(self.appointment_screen_page.TO_DO_TAB, 1)
 
     
 ##TEST
